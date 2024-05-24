ae2aee583699a50c6375a8e5b52b05cdb10384b6
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-05-24 14:50:28 -0500

    Add urls and fix settings

--- main/models.py
+++ main/models.py
@@ -1,26 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class ArtPiece(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     artist_name = models.CharField(max_length=200)
     piece_name = models.CharField(max_length=300)
     piece_description = models.TextField()
     link = models.URLField(blank=True, null=True)
     approved_status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.piece_name
 
 
 class SentArtPiece(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     art_piece = models.ForeignKey(ArtPiece, on_delete=models.CASCADE)
     sent_date = models.DateField()
 
-    # class Meta:
-    # unique_together = ('user', 'art_piece')
+    class Meta:
+        unique_together = ('user', 'art_piece')
 
     def __str__(self):
         return f'{self.user} - {self.art_piece}'
