06c4e74e15c9d4a7a6128c8d19f323e5d78e1862
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-05-24 13:48:57 -0500

    Fix app name

--- main/models.py
+++ main/models.py
@@ -2,20 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-
 class ArtPiece(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     artist_name = models.CharField(max_length=200)
     piece_name = models.CharField(max_length=300)
     piece_description = models.TextField()
@@ -27,12 +15,12 @@ class ArtPiece(models.Model):
 
 
 class SentArtPiece(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     art_piece = models.ForeignKey(ArtPiece, on_delete=models.CASCADE)
     sent_date = models.DateField()
 
-    class Meta:
-        unique_together = ('user', 'art_piece')
+    # class Meta:
+    # unique_together = ('user', 'art_piece')
 
     def __str__(self):
         return f'{self.user} - {self.art_piece}'
