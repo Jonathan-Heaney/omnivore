6a6ae7ab24c1fb18cb88ad7f19faa615700c79a0
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-05-27 16:17:02 -0500

    Created admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ArtPiece, SentArtPiece
 
-# Register your models here.
+
+class ArtPieceAdmin(admin.ModelAdmin):
+    list_filter = ("user", "approved_status")
+    list_display = ("piece_name", "artist_name", "link",
+                    "approved_status", "created_at")
+
+
+admin.site.register(ArtPiece, ArtPieceAdmin)
+admin.site.register(SentArtPiece)
