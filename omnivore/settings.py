3f6aba2a09dea914c305b132460221771a1b739e
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-06-04 12:16:10 -0500

    Change location of static files

--- omnivore/settings.py
+++ omnivore/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
