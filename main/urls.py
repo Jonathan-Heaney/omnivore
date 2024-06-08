deaf83509dde55c21a18e05455184b8ce6630ca0
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-06-08 11:24:52 -0500

    Remove send-art-piece-email URL

--- main/urls.py
+++ main/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("share-art", views.share_art, name="share_art"),
     path("my-shared-art", views.my_shared_art, name="my_shared_art"),
     path("my-received-art", views.my_received_art, name="my_received_art"),
-    path('send-art-piece-email/<int:user_id>',
-         views.send_art_piece_email, name='send_art_piece_email'),
     path('edit-art-piece/<int:pk>', views.edit_art_piece, name='edit_art_piece'),
     path('delete-art-piece/<int:pk>',
          views.delete_art_piece, name='delete_art_piece'),
