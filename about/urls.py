738ca2a6bba4ea5a2c6070d985ba52b621729a32
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-06-12 16:56:22 -0500

    Adjust home page, add FAQ page

--- about/urls.py
+++ about/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('how-it-works', views.how_it_works, name='how_it_works'),
+    path('faq', views.faq, name='faq'),
     path('submission-guidelines', views.submission_guidelines,
          name='submission_guidelines'),
 ]
--- about/views.py
+++ about/views.py
@@ -6,6 +6,10 @@ def how_it_works(request):
     return render(request, 'about/how_it_works.html')
 
 
+def faq(request):
+    return render(request, 'about/faq.html')
+
+
 @login_required(login_url="/login")
 def submission_guidelines(request):
     return render(request, 'about/submission_guidelines.html')
