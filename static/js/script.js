303636b4628de99880518331f626d8ae03f980af
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-07-30 11:44:48 -0500

    Restyle and fix modal

--- static/js/script.js
+++ static/js/script.js
@@ -53,3 +53,42 @@ function confirmDelete() {
     'Are you sure you want to delete this post? This cannot be undone.'
   );
 }
+
+// Modal handling
+document.addEventListener('DOMContentLoaded', function () {
+  // Function to open the modal
+  function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+  }
+
+  // Function to close the modal
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+  }
+
+  // Add event listeners to all open modal links
+  document.querySelectorAll('.open-modal').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent default action that causes scrolling
+      const modalId = event.target.getAttribute('data-modal-id');
+      openModal(modalId);
+    });
+  });
+
+  // Add event listeners to all close modal spans
+  document.querySelectorAll('.modal .close').forEach(function (element) {
+    element.addEventListener('click', function () {
+      const modalId = element.getAttribute('data-modal-id');
+      closeModal(modalId);
+    });
+  });
+
+  // Close the modal when clicking outside of the modal content
+  window.addEventListener('click', function (event) {
+    document.querySelectorAll('.modal').forEach(function (modal) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
+});
