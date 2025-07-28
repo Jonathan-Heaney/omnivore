d0ccd32fc4ff97a9d6f3f21a66e3ff034133f1d2
Jonathan Heaney <jonathan.heaney@gmail.com>
2025-07-28 12:31:33 -0500

    Revert settings/env file to remove dj_database_url

--- omnivore/settings.py
+++ omnivore/settings.py
@@ -3,24 +3,33 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 import warnings
-import dj_database_url
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
+# Figure out which environment we're in
+current_environment = os.getenv("ENVIRONMENT", "production")
 
-if ENVIRONMENT == "staging":
-    load_dotenv('.env.staging')
-    DEBUG = True
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
+# Load the appropriate .env file based on the environment
+if current_environment == "staging":
+    load_dotenv(".env.staging")
 else:
-    load_dotenv('.env.production')
-    DEBUG = False
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
+    load_dotenv(".env.production")
 
-print(os.getenv("STAGING_DATABASE_URL"))
+
+DB_NAME = os.environ.get('DB_NAME')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USERNAME,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
 
 # Amazon SES configuration
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
