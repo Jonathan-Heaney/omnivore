044b75c5ffdfe8fedf4c186538eb0b999bb90b3c
Jonathan Heaney <jonathan.heaney@gmail.com>
2025-08-20 21:58:01 -0500

    Remove requirement to submit something that month

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,17 +3,6 @@ from django.utils import timezone
 from .models import ArtPiece, Notification
 
 
-def has_submitted_art_this_month(request):
-    if request.user.is_authenticated:
-        today = timezone.now().date()
-        first_day_of_month = timezone.make_aware(
-            timezone.datetime(today.year, today.month, 1))
-        has_submitted = ArtPiece.objects.filter(
-            user=request.user, created_at__gte=first_day_of_month).exists()
-        return {'has_submitted_art_this_month': has_submitted}
-    return {'has_submitted_art_this_month': False}
-
-
 def notifications_context(request):
     if request.user.is_authenticated:
         return {
--- omnivore/settings.py
+++ omnivore/settings.py
@@ -113,7 +113,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'main.context_processors.has_submitted_art_this_month',
                 'main.context_processors.notifications_context',
             ],
         },
