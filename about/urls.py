26835299df916058ac3cfd75b8b3d3d87755e85f
Jonathan Heaney <jonathan.heaney@gmail.com>
2025-09-09 21:25:58 -0500

    Remove FAQ page

--- about/urls.py
+++ about/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('faq', views.faq, name='faq'),
     path('submission-guidelines', views.submission_guidelines,
          name='submission_guidelines'),
 ]
