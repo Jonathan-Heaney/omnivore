d4a081bf40cced48704ead5091062ffb69d7ff15
Jonathan Heaney <jonathan.heaney@gmail.com>
2025-09-11 12:49:25 -0500

    Change Debug to false

--- omnivore/settings.py
+++ omnivore/settings.py
@@ -66,13 +66,11 @@ CELERY_TASK_ROUTES = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get('DEBUG')
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 
